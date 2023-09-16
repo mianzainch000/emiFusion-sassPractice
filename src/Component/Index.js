d31c13ae4a5814e620a99f1716cd0cedc324b137
mianzainch000 <mianzainch000@gmail.com>
2023-09-16 12:27:05 +0500

    initial commit

---
+++ src/Component/Index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../scss/main.scss";
+
+export const Index = () => {
+  return (
+    <div className="light">
+      <h1>SCSS Practice</h1>
+      <div className="mainPara">
+        <p className="historyofPak1">
+          The history of preceding the country's independence in 1947[1] is
+          shared with that of Afghanistan, India, and Iran. Spanning the western
+          expanse of the Indian subcontinent and the eastern borderlands of the
+          Iranian plateau, the region of present-day Pakistan served both as the
+          fertile ground of a major civilization and as the gateway of South
+          Asia to Central Asia{" "}
+        </p>
+        <p className="historyofPak2">
+          The history of preceding the country's independence in 1947[1] is
+          shared with that of Afghanistan, India, and Iran. Spanning the western
+          expanse of the Indian subcontinent and the eastern borderlands of the
+          Iranian plateau, the region of present-day Pakistan served both as the
+          fertile ground of a major civilization and as the gateway of South
+          Asia to Central Asia{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
